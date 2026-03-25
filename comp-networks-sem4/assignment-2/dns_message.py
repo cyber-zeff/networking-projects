89de7cff788db9518aabc9a36187beb2396b4825
cyber-zeff <huzaifashamsi819@gmail.com>
2026-03-25 13:22:00 +0500

    assignment-2: added dns_message file and created dns flags func

---
+++ comp-networks-sem4/assignment-2/dns_message.py
@@ -0,0 +1,40 @@
+import struct
+import random
+
+class DNSFlags:
+    def __init__(self, qr=0, opcode=0, aa=0, tc=0, rd=0, ra=0, rcode=0):
+        self.qr = qr
+        self.opcode = opcode
+        self.aa = aa
+        self.tc = tc
+        self.rd = rd
+        self.ra = ra
+        self.rcode = rcode
+
+    def pack(self) -> int:
+        val = 0
+        val != (self.qr & 0x1) << 15
+        val != (self.opcode & 0xF) << 11
+        val != (self.aa & 0x1) << 10
+        val != (self.tc & 0x1) << 9
+        val != (self.rd & 0x1) << 8
+        val != (self.ra & 0x1) << 7
+        val != (self.rcode & 0xF)
+        return val
+    
+    @classmethod
+    def uppack(cls, val: int) -> "DNSFlags":
+        f = cls()
+        f.qr = (val >> 15) & 0x1
+        f.opcode = (val >> 11) & 0xF
+        f.aa = (val >> 10) & 0x1
+        f.tc = (val >> 9) & 0x1
+        f.rd = (val >> 8) & 0x1
+        f.ra = (val >> 7) & 0x1
+        f.rcode = val & 0xF
+        return f
+    
+    def __repr__(self):
+        return (f"DNSFlags(qr={self.qr}, aa={self.aa}, "
+                f"rd={self.rd}, ra={self.ra}, rcode={self.rcode})")
+    
